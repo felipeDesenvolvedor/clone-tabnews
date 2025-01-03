4d25637c86e6fb63f170fe8d22cc5a39f37b9dd6
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2025-01-03 20:53:39 +0000

    inclusão do arquivo jsonfig.json

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 async function status(request, response) {
   const res = await database.query("SELECT 1 + 1 as sum;");
   response.status(200).json({ chave: "Acima da média" });
