176596a2669bbaa586f7d32c21753d65b5324d36
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2025-01-21 22:41:23 +0000

    Adicionando tratamento de erro na chamado do banco

--- infra/database.js
+++ infra/database.js
@@ -8,12 +8,22 @@ async function query(querySearch) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
+  // Nessas variaveis de ambiente,
+  // viram os valores de onde a aplicação estiver hospedada ou rodando
+
+  console.log("Credenciais do postgres", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
   try {
     await client.connect();
     const res = await client.query(querySearch);
     return res;
   } catch (error) {
-    return error;
+    throw error;
   } finally {
     await client.end();
   }
