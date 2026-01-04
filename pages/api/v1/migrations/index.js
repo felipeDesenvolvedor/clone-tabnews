4f6eb446329bc48fbdd5675b60dd046bc268aa81
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2026-01-04 21:37:07 +0000

    migrations no modo `dryRun`

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,15 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+
+// vai retornar as migrations que ainda não foram aplicadas
+export default async function migrations(request, response) {
+  const migrations = await migrationRunner({
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  });
+  response.status(200).json(migrations);
+}
