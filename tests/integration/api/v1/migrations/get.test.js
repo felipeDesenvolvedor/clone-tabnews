4379819d94a1476f0695789ea8e0061c3d663b60
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2026-01-05 18:29:08 +0000

    Resolucao Desafio 1 e 2: icluido o arquivo  e  com config nextJest e dir ./

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,3 +1,5 @@
+import database from "infra/database.js";
+
 test("Get to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
@@ -5,5 +7,8 @@ test("Get to /api/v1/migrations should return 200", async () => {
   const responseBody = await response.json();
   console.log(responseBody);
 
+  console.log("process.env in test", process.env);
+
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
