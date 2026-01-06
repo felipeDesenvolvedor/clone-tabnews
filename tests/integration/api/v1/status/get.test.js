5b7bd48d46580bb08d0bbb4b09a4c7dbdca9cb72
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2026-01-06 14:18:25 +0000

    voltando as config de teste do db local

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,6 +1,5 @@
 test("Get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  console.log("response", response);
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
@@ -9,10 +8,10 @@ test("Get to /api/v1/status should return 200", async () => {
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parseUpdatedAt);
   expect(responseBody.postgres_version).toBeDefined();
-  expect(responseBody.postgres_version).toEqual(18.0);
+  expect(responseBody.postgres_version).toEqual(16.0);
   expect(responseBody.postgres_max_connections).toEqual(
     responseBody.postgres_max_connections,
   );
-  expect(responseBody.postgres_used_connections).toEqual(2);
+  expect(responseBody.postgres_used_connections).toEqual(1);
   expect(responseBody).not.toHaveProperty("password");
 });
