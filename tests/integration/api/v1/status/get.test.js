c5ab63b83d2cc1eaa3cf3ea051197bef5cd9a9e2
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2026-02-06 12:15:37 -0300

    test: rename all descriptions to use nem pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,20 +4,24 @@ beforeAll(async () => {
   await orchestrator.awaitForAllServices();
 });
 
-test("Get to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymouns user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
 
-  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
-  expect(responseBody.postgres_version).toBeDefined();
-  expect(responseBody.postgres_version).toEqual(16.0);
-  expect(responseBody.postgres_max_connections).toEqual(
-    responseBody.postgres_max_connections,
-  );
-  expect(responseBody.postgres_used_connections).toEqual(1);
-  expect(responseBody).not.toHaveProperty("password");
+      const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+      expect(responseBody.postgres_version).toBeDefined();
+      expect(responseBody.postgres_version).toEqual(16.0);
+      expect(responseBody.postgres_max_connections).toEqual(
+        responseBody.postgres_max_connections,
+      );
+      expect(responseBody.postgres_used_connections).toEqual(1);
+      expect(responseBody).not.toHaveProperty("password");
+    });
+  });
 });
