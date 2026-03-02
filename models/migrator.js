9be1b14b57d3eb5b0983c54f59c8e0a46b7aa060
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2026-03-02 20:08:34 +0000

    feat: hide `migrator` logs

--- models/migrator.js
+++ models/migrator.js
@@ -7,7 +7,7 @@ const migrationsRunnerConfig = {
   dryRun: true,
   dir: resolve("infra", "migrations"),
   direction: "up",
-  verbose: true,
+  log: () => {},
   migrationsTable: "pgmigrations",
 };
 
