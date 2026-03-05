975ce26f4b3a610fecb078a7babd6c651f7c781c
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2026-03-05 16:48:01 -0300

    feat: add validateUniqueUsername in model users

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ import { ValidationError } from "infra/errors.js";
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
+  await validateUniqueUsername(userInputValues.username);
 
   const newUser = await runInsertQuery(userInputValues);
   return newUser;
@@ -24,6 +25,27 @@ async function create(userInputValues) {
       throw new ValidationError({
         message: "O email informado já está sendo utilizado.",
         action: "Ultilize outro email para realizar o cadastro.",
+      });
+    }
+  }
+
+  async function validateUniqueUsername(username) {
+    const results = await database.query({
+      text: `
+        SELECT
+          username
+        FROM
+          users
+        WHERE
+          LOWER(username) = LOWER($1);
+      `,
+      values: [username],
+    });
+
+    if (results.rowCount > 0) {
+      throw new ValidationError({
+        message: "O usuário informado já está sendo ultilizado.",
+        action: "Ultilize outro usuário para realizar o cadastro.",
       });
     }
   }
